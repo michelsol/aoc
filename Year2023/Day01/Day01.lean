61acda8a1fab2ac52fb1be94d99a5fc135291f8c
michelsol <michel.ad.code@gmail.com>
2023-12-26 15:14:09 +0100

    update

--- Year2023/Day01/Day01.lean
+++ Year2023/Day01/Day01.lean
@@ -1,4 +1,4 @@
--- import Std
+import Std
 
 def calibration (s : String) : Option Nat :=
   let l := (s.data.filter Char.isDigit).map λ c => c.toNat - '0'.toNat
@@ -11,25 +11,30 @@ def part1 (lines : Array String) := do
   let l ← lines.mapM calibration
   return l.foldr (. + .) 0
 
-def preprocess (s : String) : String := -- this is a wrong idea
-  let s := s.replace "one" "1"
-  let s := s.replace "two" "2"
-  let s := s.replace "three" "3"
-  let s := s.replace "four" "4"
-  let s := s.replace "five" "5"
-  let s := s.replace "six" "6"
-  let s := s.replace "seven" "7"
-  let s := s.replace "eight" "8"
-  let s := s.replace "nine" "9"
-  s
+def String.reverse (s : String) := String.mk s.data.reverse
+def numbers := #["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+def numbers2 := ((Array.range 9).map (. + 1)).map λ k => (s!"{k}", k)
+def ns := numbers.mapIdx λ k x => (x, k.val + 1)
+def nsRev := (numbers.map String.reverse).mapIdx λ k x => (x, k.val + 1)
 
-def part2 (lines : Array String) :=
-  lines.map preprocess
+def calibration2 (s : String) : Option Nat :=
+  let sr := s.reverse
+  let srlength := sr.length
+  let a := (ns ++ numbers2).filterMap λ (ds, d) => (s.findSubstr? ds).map λ x => (x.startPos.1, d)
+  let b := (nsRev ++ numbers2).filterMap λ (ds, d) => (sr.findSubstr? ds).map λ x => (srlength - x.startPos.1, d)
+  let _ : Ord (Nat × Nat) := lexOrd
+  do
+    let first ← a.min?
+    let last ← b.max?
+    return first.2 * 10 + last.2
+
+def part2 (lines : Array String) := do
+  let l ← lines.mapM calibration2
+  return l.foldr (. + .) 0
 
 def main : IO Unit := do
-  let lines ← IO.FS.lines "Year2023/Day01/ex1.txt"
+  let lines ← IO.FS.lines "Year2023/Day01/in.txt"
   IO.println s!"part1: {part1 lines}"
-  let lines ← IO.FS.lines "Year2023/Day01/ex2.txt"
   IO.println s!"part2: {part2 lines}"
 
 -- #eval main
