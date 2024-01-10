2fe347d5dd3e29cfab9d0831a9cbe2cb4dc57ca1
michelsol <michel.ad.code@gmail.com>
2024-01-10 18:15:31 +0100

    update

--- Aoc/Basic.lean
+++ Aoc/Basic.lean
@@ -27,4 +27,61 @@ def splitOnP (P : α → Bool) (a : Array α) :=
 
 def drop (k : Nat) (a : Array α) := a.toList.drop k |>.toArray
 
+open Prod in
+def makeProds (l : Array α) :=
+  let a := l.zip (range l.size) |>.filter (snd . % 2 = 0) |>.map fst
+  let b := l.zip (range l.size) |>.filter (snd . % 2 = 1) |>.map fst
+  a.zip b
+
+-- #eval #['A', 'C', 'Z', 'E', 'F', 'Y'].makeProds
+
 end Array
+
+
+structure Interval where
+  first : Nat
+  firstOut : Nat
+deriving Inhabited
+instance : ToString Interval where
+  toString i := s!"[{i.1}, {i.2})"
+
+namespace Interval
+
+abbrev isEmpty (i : Interval) := i.2 <= i.1
+
+def intersect (i j : Interval) : Interval := ⟨max i.1 j.1, min i.2 j.2⟩
+
+def sortByFirst (a : Array Interval) := a.qsort λ i j => i.1 < j.1
+
+-- assumption : a has sorted and disjoint intervals. Same for b
+-- output will have sorted and disjoint intervals
+def intersectArrays (a b : Array Interval) : Array Interval := Id.run do
+  let mut r := #[]
+  let mut i := 0
+  let mut j := 0
+  while i < a.size ∧ j < b.size do
+    let lo := max a[i]!.1 b[j]!.1
+    let hi := min a[i]!.2 b[j]!.2
+    if lo < hi then r := r.push ⟨lo, hi⟩
+    if a[i]!.2 < b[j]!.2 then i := i + 1
+    else j := j + 1
+  return r
+
+def ensureDisjoint (a : Array Interval) : Array Interval := Id.run do
+  let mut last := a[0]!
+  let mut r := #[]
+  for i in a.drop 1 do
+    if last.2 < i.1 then
+      r := r.push last
+      last := i
+    else
+      last := ⟨last.1, max last.2 i.2⟩
+  r := r.push last
+  return r
+
+-- def list1 : Array Interval := #[⟨0, 2⟩, ⟨5, 10⟩, ⟨13, 23⟩, ⟨24, 25⟩]
+-- def list2 : Array Interval := #[⟨1, 5⟩, ⟨8, 12⟩, ⟨15, 24⟩, ⟨25, 26⟩]
+-- #eval intersectArrays list1 list2 |> sortByFirst
+-- #eval ensureDisjoint #[⟨0, 1⟩, ⟨2, 12⟩, ⟨13, 23⟩, ⟨24, 25⟩]
+
+end Interval
--- Year2023/Day05/Day05.lean
+++ Year2023/Day05/Day05.lean
@@ -2,32 +2,35 @@ import Std
 import Aoc
 
 structure MapEntry where
-  dst : Nat
-  src : Nat
-  len : Nat
-deriving Repr, Inhabited
-instance : ToString MapEntry := ⟨reprStr⟩
+  dst : Interval
+  src : Interval
+deriving Inhabited
+instance : ToString MapEntry where
+  toString i := s!"\{{i.src} => {i.dst}}"
 
 def String.toMapEntry (s : String) : MapEntry :=
   let x := s.splitOn " " |>.map String.toNat!
-  { dst := x[0]!
-    src := x[1]!
-    len := x[2]! }
+  { dst := ⟨x[0]!, x[0]! + x[2]!⟩
+    src := ⟨x[1]!, x[1]! + x[2]!⟩}
 
 def parseMaps (lines : Array String) :=
     lines.filter (λ l => !l.isEmpty) |>.splitOnP (λ l => l.contains ':') |>.drop 2
-    |>.map λ m => m.map String.toMapEntry
+    |>.map λ m =>
+      (m.map String.toMapEntry).qsort λ a b => a.src.1 < b.src.1
+
+def parseSeeds (line : String) :=
+  line.splitOn " " |>.drop 1 |>.toArray |>.map String.toNat!
 
 def mapElement (x : Nat) (map : Array MapEntry) : Nat := Id.run do
   let mut r := x
   for e in map do
-    if r ≥ e.src ∧ r < e.src + e.len then
-      r := (r - e.src) + e.dst
+    if r ≥ e.src.1 ∧ r < e.src.2 then
+      r := r - e.src.1 + e.dst.1
       break
   return r
 
 def part1 (lines : Array String) :=
-  let seeds := lines[0]!.splitOn " " |>.drop 1 |>.toArray |>.map String.toNat!
+  let seeds := parseSeeds lines[0]!
   let maps := parseMaps lines
   let locations := seeds.map λ seed => Id.run do
     let mut r := seed
@@ -36,27 +39,46 @@ def part1 (lines : Array String) :=
     return r
   locations.min?.get!
 
--- implicit assumption: map is sorted and contains disjoint intervals
-def mapInterval (a b : Nat) (map : Array MapEntry) : Array (Nat × Nat) := Id.run do
-  let mut r := #[]
-  let mut x := a
-  for e in map do
-    let aj := e.src
-    let bj := aj + e.len
-    let aj' := e.dst
-    let bj' := aj' + e.len
-    -- unfinished
-    if x < aj then
-      r := r.push (x - aj + aj', bj - aj + aj')
-      x := aj
-    if x ≥ aj ∧ x < bj then
-      r := r.push (x - aj + aj', bj - aj + aj')
-      x := bj
-  return r
+def parseSeedRanges (line : String) : Array Interval :=
+  line.splitOn " " |>.drop 1 |>.toArray |>.map String.toNat!
+    |>.makeProds |>.map (λ ⟨a, b⟩ => ⟨a, a + b⟩) |>.qsort λ a b => a.1 < b.1
 
+def mapArray (a : Array Interval) (b : Array MapEntry) : Array Interval := Id.run do
+  let mut r := #[]
+  let mut i := 0
+  let mut j := 0
+  let mut x := a[0]!.1
+  repeat
+    let e := b[j]!
+    let s := a[i]!
+    let new : Interval := ⟨max s.1 e.src.1, min s.2 e.src.2⟩
+    if new.1 < new.2 then
+      if x < new.1 then r := r.push ⟨x, new.1⟩
+      r := r.push ⟨new.1 - e.src.1 + e.dst.1, new.2 - e.src.1 + e.dst.1⟩
+      x := new.2
+    if j + 1 < b.size ∧ e.src.2 <= s.2 then
+      j := j + 1
+    else
+      i := i + 1
+      if x < s.2 then r := r.push ⟨x, s.2⟩
+      if i >= a.size then break
+      x := a[i]!.1
+  return r |>.qsort λ a b => a.1 < b.1
+
+def part2 (lines : Array String) :=
+  let seeds := parseSeedRanges lines[0]!
+  let maps := parseMaps lines
+  let locations := Id.run do
+    let mut r := seeds
+    for map in maps do
+      r := mapArray r map
+    return r
+  locations
+  -- locations.get! 0 |>.1
 
 def main : IO Unit := do
-  let lines ← IO.FS.lines "Year2023/Day05/ex1.txt"
-  IO.println s!"part1: {part1 lines}"
+  let lines ← IO.FS.lines "Year2023/Day05/in1.txt"
+  -- IO.println s!"part1: {part1 lines}"
+  IO.println s!"part2: {part2 lines}"
 
 -- #eval timeit "main : " $ main
